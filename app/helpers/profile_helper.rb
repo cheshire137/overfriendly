fad9df757d29abc6ce3840e1487c7a0e50d04949
Sarah Vessels <cheshire137@gmail.com>
2018-01-28 11:46:28 -0600

    Show TL;DR on profile page

--- app/helpers/profile_helper.rb
+++ app/helpers/profile_helper.rb
@@ -1,7 +1,8 @@
 module ProfileHelper
   def hero_bar_width(seconds, max, total)
-    percent = (seconds.to_f / total) * 100
+    hero_percent = (seconds.to_f / total) * 100
     max_percent = (max.to_f / total) * 100
-    (percent / max_percent) * 100
+    percent = (hero_percent / max_percent) * 100
+    percent + 2
   end
 end
