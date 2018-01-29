e6b7199cf12e6f098b3a06020c35a2b2f7d049c6
Sarah Vessels <cheshire137@gmail.com>
2018-01-28 18:50:50 -0600

    Ensure login when viewing team

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,4 +1,7 @@
 class TeamsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_user_complete, only: [:show]
+
   def show
     @team = Team.friendly.find(params[:slug])
     @players = @team.players.includes(:user)
