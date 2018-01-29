40dada5093df800bb63f22e0958c445c201b270d
Sarah Vessels <cheshire137@gmail.com>
2018-01-29 10:26:34 -0600

    Don't require auth before showing team

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,5 +1,5 @@
 class ProfileController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: :refresh
 
   def show
     profile = get_profile
--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,7 +1,4 @@
 class TeamsController < ApplicationController
-  before_action :authenticate_user!
-  before_action :ensure_user_complete, only: [:show]
-
   def show
     @team = Team.friendly.find(params[:slug])
     @players = @team.players.includes(:user)
