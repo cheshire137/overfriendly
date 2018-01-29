726055af75bdfa3c0ce7234b83a897d4fef06401
Sarah Vessels <cheshire137@gmail.com>
2018-01-29 10:27:52 -0600

    Don't require auth to view user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:show]
   before_action :ensure_user_complete, only: [:show]
 
   def index
